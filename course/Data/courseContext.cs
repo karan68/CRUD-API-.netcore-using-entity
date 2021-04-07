6bb510290489d8caf020793f4e00dafd7d649e17
karan yadav <47856276+karan68@users.noreply.github.com>
2021-04-07 19:21:50 +0530

    Add files via upload

---
+++ course/Data/courseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using course.Models;
+
+namespace course.Data
+{
+    public class courseContext : DbContext
+    {
+        public courseContext (DbContextOptions<courseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<course.Models.Course> Course { get; set; }
+    }
+}
